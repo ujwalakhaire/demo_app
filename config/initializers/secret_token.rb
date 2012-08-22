fc412fb96d631a87d11b2ce48c8e04397f86571f
Arnab Deka <arnab.deka@livingsocial.com>
2012-08-22 12:59:54 +0530

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '374b64dbb9730d96be122ebfb6966636dc3fd422605d2648afece4762f4ff1fe377314c979ec349520ee5547586da6f37250fe43277932fa20c35b33d72e369f'
